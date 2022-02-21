cbe45886da370db3f62b11c7268a9dc4d4ce6906
Raymond Chou <raymond.chou@mail.utoronto.ca>
2022-02-21 17:29:03 -0500

    add google analytics and offline data persistence

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import './App.css';
-import { getFirestore } from 'firebase/firestore';
-import { FirestoreProvider, useFirebaseApp } from 'reactfire';
+
+import { initializeFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
+import { useInitFirestore, FirestoreProvider, AnalyticsProvider, useAnalytics, useFirebaseApp } from 'reactfire';
+import { getAnalytics, logEvent } from 'firebase/analytics';
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+
 // import DenseTable from './components/DenseTable'
 import Table from './components/Table'
 import SearchAppBar from './components/SearchBarApp';
+import CircularProgress from '@mui/material/CircularProgress';
 
-function App() {
-  const firestoreInstance = getFirestore(useFirebaseApp());
-  return (
-    <FirestoreProvider sdk={firestoreInstance}>
-      <SearchAppBar />
-      <Table />
-    </FirestoreProvider>
-  );
+function MyPageViewLogger() {
+  const analytics = useAnalytics();
+  const location = useLocation()
+  // By passing `location.pathname` to the second argument of `useEffect`,
+  // we only log on first render and when the `pathname` changes
+  useEffect(() => {
+    logEvent(analytics, 'page_view', { page_location: location.pathname });
+  }, [location.pathname]);
+
+  return null;
 }
 
-export default App;
+export default function App() {
+  const { status, data: firestoreInstance } = useInitFirestore(async (firebaseApp) => {
+    const db = initializeFirestore(firebaseApp, {});
+    await enableIndexedDbPersistence(db);
+    return db;
+  });
+  const app = useFirebaseApp();
+  // firestore init isn't complete yet
+  if (status === 'loading') {
+    return <CircularProgress />;
+  }
+
+  // pass the Firestore instance to FirestoreProvider
+  // now we can be sure that any child of FirestoreProvider
+  // has a fully initialized Firestore instance with
+  // indexedDbPersistence enabled
+  return (
+    <AnalyticsProvider sdk={getAnalytics(app)}>
+      <FirestoreProvider sdk={firestoreInstance}>
+        <SearchAppBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Table />} />
+          </Routes>
+          <MyPageViewLogger />
+        </BrowserRouter>
+      </FirestoreProvider>
+    </AnalyticsProvider>
+
+  );
+}
