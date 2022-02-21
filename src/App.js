5fad8427b03071f8fb1d51aa3bea9ba81c29e26b
Raymond Chou <raymond.chou@mail.utoronto.ca>
2022-02-21 15:22:38 -0500

    implemented data table

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import { doc, getFirestore } from 'firebase/firestore';
-import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
-
-function BurritoTaste() {
-  // easily access the Firestore library
-  const burritoRef = doc(useFirestore(), 'Securities', 'A US Equity');
-  // subscribe to a document for realtime updates. just one line!
-  const { status, data } = useFirestoreDocData(burritoRef);
-  // easily check the loading status
-  if (status === 'loading') {
-    return <p>Fetching burrito flavor...</p>;
-  }
-  // return <p>The burrito is CINDY{data['Short Name']}!</p>;
-  return <p>The burrito is CINDY</p>;
-
-}
+import { getFirestore } from 'firebase/firestore';
+import { FirestoreProvider, useFirebaseApp } from 'reactfire';
+import DenseTable from './components/DenseTable'
 
 function App() {
   const firestoreInstance = getFirestore(useFirebaseApp());
   return (
     <FirestoreProvider sdk={firestoreInstance}>
-      <h1>🌯</h1>
-      <BurritoTaste />
+      <h1>Project ESG</h1>
+      <DenseTable />
     </FirestoreProvider>
   );
 }
